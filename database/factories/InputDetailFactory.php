eae3588cad3ca6a1e675edfc73f40dab9f354f33
Lee <279250195@qq.com>
2017-11-21 17:06:01 +0900

    add factory and migration

---
+++ database/factories/InputDetailFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\InputDetail::class, function (Faker $faker) {
+
+    return [
+        'amount' => $faker->randomFloat($nbMaxDecimals = 2, $min = 0, $max = 1000),
+        'category_id' => $faker->numberBetween($min = 1, $max = 3) ,
+		'consumption_flag' => $faker->numberBetween($min = 0, $max = 1) ,
+		'currency_id' => $faker->numberBetween($min = 1, $max = 5) ,
+		'location' => $faker->streetName ,
+		'comment' => $faker->jobTitle ,
+		'image_url' => $faker->url ,
+    ];
+});
---
+++ database/migrations/2017_11_21_071529_create_inputDetails_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInputDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('input_details', function (Blueprint $table) {
+            $table->increments('id');
+			$table->double('amount');
+			$table->integer('category_id');
+			$table->integer('consumption_flag');
+			$table->integer('currency_id');
+			$table->string('location');
+			$table->string('comment');
+			$table->string('image_url');
+			$table->integer('delete_flag')->default=0;
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('input_details');
+    }
+}
